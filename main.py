0dc84227c89b53a41336d9c15cad58ae8d81646c
404 <sametalpdeger9@gmail.com>
2025-06-18 20:26:25 +0300

    feat: Input option to specify which event will receive the response

--- main.py
+++ main.py
@@ -6,13 +6,16 @@ from routes.stream import stream_route
 from flask_socketio import SocketIO, join_room
 from flask_cors import CORS
 
+
 def create_app():
     app = Flask(__name__)
-    socketio = SocketIO(app,
-                       cors_allowed_origins=conf["cors_origins"],
-                       async_mode='eventlet',
-                       logger=True,
-                       engineio_logger=True)
+    socketio = SocketIO(
+        app,
+        cors_allowed_origins=conf["cors_origins"],
+        async_mode='eventlet',
+        logger=True,
+        engineio_logger=True
+    )
     CORS(app, resources={r"/*": {"origins": conf["cors_origins"]}})
 
     gemini_pool = Pool(
@@ -22,10 +25,10 @@ def create_app():
 
     @app.route('/client')
     def client():
-       if (conf['allow_clients']):
-           return render_template('index.html')
-       else:
-           return 404
+        if (conf['allow_clients']):
+            return render_template('index.html')
+        else:
+            return 404
 
     @socketio.on('connect', namespace='/')
     def handle_connect():
@@ -44,6 +47,6 @@ app, socketio = create_app()
 if __name__ == '__main__':
     logger.info('Starting Flask server at http://localhost:' + str(conf["port"]))
     socketio.run(app=app,
-                host='0.0.0.0',  # Add this if you need external access
-                port=conf["port"],
-                debug=True)
+                 host='0.0.0.0',  # Add this if you need external access
+                 port=conf["port"],
+                 debug=True)
--- routes/stream.py
+++ routes/stream.py
@@ -6,9 +6,13 @@ from pydantic import BaseModel
 
 from Logger import logger
 
+
 class Input(BaseModel):
     prompt: str
-    model: Literal['gemma-3-1b-it', 'gemma-3-27b-it', 'gemma-3-4b-it', 'gemma-3-12b-it', 'gemma-3n-e4b-it' ,'gemini-2.0-flash', 'gemini-2.0-flash-lite', 'gemini-2.5-pro-preview-06-05', 'gemini-1.5-pro']
+    model: Literal[
+        'gemma-3-1b-it', 'gemma-3-27b-it', 'gemma-3-4b-it', 'gemma-3-12b-it', 'gemma-3n-e4b-it', 'gemini-2.0-flash', 'gemini-2.0-flash-lite', 'gemini-2.5-pro-preview-06-05', 'gemini-1.5-pro']
+    sio_event: str
+
 
 def stream_route(
         io: SocketIO,
@@ -23,10 +27,10 @@ def stream_route(
 
             for chunk in gemini_pool.getResponseStream(model=input.model, contents=input.prompt):
                 if chunk.text:
-                    io.emit('stream', chunk.text, namespace='/')
+                    io.emit(input.sio_event, chunk.text, namespace='/', to=session_id)
                     io.sleep(0.1)
 
             io.emit('stream-success', namespace='/', to=session_id)
         except Exception as e:
-            io.emit('stream-error', namespace='/', to=session_id)
+            io.emit('stream-error', str(e), namespace='/', to=session_id)
             logger.error(f'Route Error: /stream - {str(e)}')
